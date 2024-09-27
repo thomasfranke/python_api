d868e48aa24889174299f0809e88b433953c4d31
Thomas Franke <thomasfranke@gmail.com>
2024-09-26 22:07:40 -0300

    feat(endpoints) criacao de novos endpoints

--- myproject/crud1/models.py
+++ myproject/crud1/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class Pessoa(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=100)
-    dt_nasc = models.DateField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
