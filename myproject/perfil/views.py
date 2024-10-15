22c4261c86d4edf0d5e78fbce4fec10f7d1fe282
guilopesouza <guilolsouza@gmail.com>
2024-10-15 12:00:01 -0300

    Implementa views para ver e editar perfil

---
+++ myproject/perfil/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Perfil
+from .forms import PerfilForm
+
+# View to display a user profile
+def ver_perfil(request, id):
+    perfil = get_object_or_404(Perfil, id=id)
+    return render(request, 'perfil/ver_perfil.html', {'perfil': perfil})
+
+# View to edit a user profile
+def editar_perfil(request):
+    perfil = Perfil.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = PerfilForm(request.POST, instance=perfil)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_perfil', id=perfil.id)
+    else:
+        form = PerfilForm(instance=perfil)
+    return render(request, 'perfil/editar_perfil.html', {'form': form})
